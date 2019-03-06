818733494e546552e64705bcbbdbbc859e8eef6e
Wayne Collier <ke4tri@gmail.com>
2019-03-06 08:52:27 -0600

    Add project files.

---
+++ BookExampleMethod/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BookExampleMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
